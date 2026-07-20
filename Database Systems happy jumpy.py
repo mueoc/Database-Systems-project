3220a95fdb9b46bf6b49e5f3da25ead245630e62
muqtada <144257713+mueoc@users.noreply.github.com>
2026-07-20 23:13:30 +0200

    i added .env

--- Database Systems happy jumpy.py
+++ Database Systems happy jumpy.py
@@ -3,26 +3,25 @@ import mysql.connector
 from flask import Flask, render_template, render_template_string
 from dotenv import load_dotenv
 
-# 1. Läs in miljövariabler från .env-filen
 load_dotenv()
 
 app = Flask(__name__)
 
-# def get_db_connection():
-#     return mysql.connector.connect(
-#         host=os.getenv('DB_HOST'),
-#         user=os.getenv('DB_USER'),
-#         password=os.getenv('DB_PASSWORD'),
-#         database=os.getenv('DB_NAME')
-#     )
-
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="botato123",
-        database="theproject"
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
     )
+
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="botato123",
+#         database="theproject"
+#     )
 
 HTML_TEMPLATE = """
 <!DOCTYPE html>
